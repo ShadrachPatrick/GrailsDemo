4e736b0c943f334465c94e004d6b1527db50a36e
Shadrach Arulrajah <shadrach@live.de>
2023-02-06 22:36:54 +0100

    wip added login feature

--- grails-app/controllers/grailsdemo/LoginController.groovy
+++ grails-app/controllers/grailsdemo/LoginController.groovy
@@ -1,43 +1,42 @@
 package grailsdemo
+import example.grails.User
 
 class LoginController {
 
-    def index() { }
+    def login() {
+        if (request.method == 'POST') {
+            def username = params.username
+            def password = params.password
 
-    // Nur eine Controller-Aktion definiert  ist Default Controller-Aktion
-    def login(String user, String password) {
-        if (user == "specht" && password == "specht") { // Login korrekt
-
-            // Eingeloggten Benutzer merken
-            session.user = user
-            session.password = password
-
-            // Navigationsherkunft abrufen und in Session-Variablen löschen
-            def fromController = session.fromController
-            def fromAction = session.fromAction
-            def fromParams = session.fromParams
-            session.fromController = null
-            session.fromAction = null
-            session.fromParams = null
-
-            if (fromController && fromAction && fromParams) {
-                redirect controller:fromController, action:fromAction, params:fromParams
+            if (username && password) {
+                // Hier könnte man eine Überprüfung gegen eine Datenbank machen
+                def user = User.findByUsernameAndPassword(username, password)
+                if (user) {
+                    session.user = user
+                    println "user found"
+                    redirect(action: 'welcome')
+                } else {
+                    println "user not found"
+                    flash.message = 'Falscher Benutzername oder Passwort'
+                    render view: '/index', model:[username:username, password:password]
+                }
             }
-            else if (fromController && fromAction) {
-                redirect controller:fromController, action:fromAction
+            else {
+                flash.message = 'Bitte Benutzername und Passwort eingeben'
+                render view: '/index', model:[username:username, password:password]
             }
-            else if (fromController) {
-                redirect controller:fromController
-            }
-            else { // Über Controllerauswahlseite eingestiegen
-                redirect uri: ""
-            }
-        } else {
-            if (user || password) { // Falscher User / Passwort bereits eingetragen
-                flash.message = "Login oder Passwort falsch!"
-            }
-            // login.gsp nochmal mit ggf. vorhandenen Eingaben rendern
-            render view: 'login', model:[user:user, password:password]
         }
     }
+
+    def welcome() {
+      if (!session.user) {
+            println "user not found"
+         redirect(action: 'welcome')
+      }
+   }
+
+   def logout() {
+      session.user = null
+      redirect(action: 'index')
+   }
 }
--- grails-app/views/index.gsp
+++ grails-app/views/index.gsp
@@ -14,17 +14,57 @@
             </li>
          </ul>
       </div>
-      <div id="list-professor" class="content" >
-      <section class="row colset-2-its">
-        <g:if test="${flash.message}">
+      <div id="list-professor" class="content mt-5 px-lg-5" >
+            <g:if test="${flash.message}">
                 <div class="message" role="status">${flash.message}</div>
             </g:if>
+        <div class="col-lg-5 col-md-6">
+            <g:form controller="login" action="login" method="post">
+                <!-- Email input -->
+                <div class="form-outline mb-4">
+                    <g:textField name="username" value="${username}"></g:textField>
+                    <label class="form-label" for="form2Example1">Username</label>
+                </div>
+
+                <!-- Password input -->
+                <div class="form-outline mb-4">
+                    <g:passwordField name="password" value="${password}"></g:passwordField>
+                    <label class="form-label" for="form2Example2">Password</label>
+                </div>
+
+                <!-- 2 column grid layout for inline styling -->
+                <div class="row mb-4">
+                    <div class="col d-flex justify-content-center">
+                    <!-- Checkbox -->
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="" id="form2Example31" checked />
+                        <label class="form-check-label" for="form2Example31"> Remember me </label>
+                    </div>
+                    </div>
+
+                    <div class="col">
+                    <!-- Simple link -->
+                    <a href="#!">Forgot password?</a>
+                    </div>
+                </div>
+
+                <!-- Submit button -->
+                <g:submitButton name="Sign in" class="btn btn-dark btn-block mb-4"/>
+
+                <!-- Register buttons -->
+                <div class="text-center">
+                    <p>Not a member? <a href="#!">Register</a></p>
+                </div>
+         </g:form>
+        </div>
+       
+<%--       <section class="row colset-2-its">
             <g:form controller="login" action="login" method="post">
                 <table>
                 <tr>
                     <td>Username:</td>
                     <td>
-                        <g:textField name="user" value="${user}"></g:textField>
+                        <g:textField name="username" value="${username}"></g:textField>
                     </td>
                 </tr>
                 <tr>
@@ -38,7 +78,6 @@
                 <g:submitButton name="Login" value="Login" />
                 </fieldset>
             </g:form>
-      </section>
       </div>
    </body>
 </html>
